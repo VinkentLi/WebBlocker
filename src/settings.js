c847c11dfb0830bee17f4acacb8890b668586a40
Vincent <vinkentclicker27@gmail.com>
2024-12-01 14:56:57 -0500

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,18 +1,7 @@
-function ensureAbsoluteUrl(input) {
-    return input.startsWith("http://") || input.startsWith("https://")
-        ? input
-        : `https://${input}`;
-}
-
-async function findRedirect(url) {
-    const axios = require('axios');
-    url = ensureAbsoluteUrl(url);
-    await axios.get(url).then(function (response) {
-        console.log("redirected url: ", response.request.responseURL);
-        url = response.request.responseURL;
-    });
-    return url;
-}
+const newWebsiteButton = document.querySelector('.newWebsiteSubmit');
+newWebsiteButton.addEventListener('click', addWebsite);
+const removeWebsiteButton = document.querySelector('.removeWebsiteSubmit');
+removeWebsiteButton.addEventListener('click', removeWebsite);
 
 async function addWebsite() {
     newWebsiteButton.disabled = true;
@@ -36,7 +25,19 @@ async function removeWebsite() {
     location.reload();
 }
 
-const newWebsiteButton = document.querySelector('.newWebsiteSubmit');
-newWebsiteButton.addEventListener('click', addWebsite);
-const removeWebsiteButton = document.querySelector('.removeWebsiteSubmit');
-removeWebsiteButton.addEventListener('click', removeWebsite);
+async function findRedirect(url) {
+    const axios = require('axios');
+    url = ensureAbsoluteUrl(url);
+    await axios.get(url).then(function (response) {
+        console.log("redirected url: ", response.request.responseURL);
+        url = response.request.responseURL;
+    });
+    return url;
+}
+
+function ensureAbsoluteUrl(input) {
+    return input.startsWith("http://") || input.startsWith("https://")
+        ? input
+        : `https://${input}`;
+}
+
