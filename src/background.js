c32ff9e27fefaf67365947f375e59c292ed97162
Vincent <vinkentclicker27@gmail.com>
2024-11-30 21:00:45 -0500

    cleaned up code

--- src/background.js
+++ src/background.js
@@ -5,53 +5,61 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.message) {
     case "set_block":
         shouldBlock = request.value;
-        chrome.tabs.query({}, function (tabs){
-            // console.log(tabs.length);
-            for (let tab of tabs) {
-                if (tab.url == null) {
-                    continue;
-                }
-                let host = new URL(tab.url).hostname;
-                // console.log(host);
-                if (blockedWebsites.includes(host)) {
-                    chrome.tabs.update(tab.id, {url: tab.url});
-                }
-            }
-        });
+        blockOrUnblockWebsites(request);
         break;
     case 'request_block':
-        // console.log(shouldBlock);
-        // console.log("sender.url: ", sender.url);
-        let host = new URL(sender.url).hostname;
-        // console.log(host);
-        if (!blockedWebsites.includes(host)) {
-            sendResponse({value: false});
-        } else {
-            sendResponse({value: shouldBlock});
-        }
-        // console.log("responded");
+        returnShouldBlock(sender, sendResponse);
         break;
     case 'request_blocked_sites':
-        // console.log("received message");
-        // console.log(blockedWebsites);
         sendResponse({value: blockedWebsites});
         break;
     case 'modify_blocked_sites':
         let website = request.value;
-        // console.log(website);
         if (request.action == 'add') {
-            if (!blockedWebsites.includes(website)) {
-                blockedWebsites.push(website);
-            }
-            sendResponse();
+            addWebsite(website);
+            sendResponse(); // dont question this
         } else if (request.action == 'remove') {
-            const index = blockedWebsites.indexOf(website);
-            if (index > -1) {
-                blockedWebsites.splice(index, 1);
-            }
+            removeWebsite(website);
             sendResponse();
         }
         break;
     }
     return true;
 });
+
+function blockOrUnblockWebsites(request) {
+    chrome.tabs.query({}, function (tabs){
+        for (let tab of tabs) {
+            if (tab.url == null) {
+                continue;
+            }
+            let host = new URL(tab.url).hostname;
+            // console.log(host);
+            if (blockedWebsites.includes(host)) {
+                chrome.tabs.update(tab.id, {url: tab.url});
+            }
+        }
+    });
+}
+
+function returnShouldBlock(sender, sendResponse) {
+    let host = new URL(sender.url).hostname;
+    if (!blockedWebsites.includes(host)) {
+        sendResponse({value: false});
+    } else {
+        sendResponse({value: shouldBlock});
+    }
+}
+
+function addWebsite(website) {
+    if (!blockedWebsites.includes(website)) {
+        blockedWebsites.push(website);
+    }
+}
+
+function removeWebsite(website) {
+    const index = blockedWebsites.indexOf(website);
+    if (index > -1) {
+        blockedWebsites.splice(index, 1);
+    }
+}
